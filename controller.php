6d8a0ed995aef36c617ace171e46c4a9735e4db6
Allan77230 <predaboss@gmail.com>
2019-02-18 15:37:30 +0100

    ajax, recherche maladie par symptome ok

--- controller.php
+++ controller.php
@@ -15,11 +15,7 @@ if( isset($_GET['message']) && trim($_GET['message']) != ''){
     traitement_message($pdo, $_GET['message']);
   }elseif($_GET['compteur'] == 1){
     echo 'en fait ca va ^^</br>';
-  }else{
-    $sth = $pdo->prepare("SELECT * FROM categorie");
-    $sth->execute();
-    $res = $sth->fetchAll();
-    var_dump($res);
+    traitement_message($pdo, $_GET['message']);
   }
 
 }else{
@@ -30,16 +26,41 @@ function traitement_message($bd, $message){
 
   $mots = explode(' ', $message);
   $noms_symptomes = array();
+  $correlation = array();
 
-  $query_symptomes = $bd->prepare("SELECT * FROM symptome");
+  $query_symptomes = $bd->prepare("SELECT name, idsymptome FROM symptome");
   $query_symptomes->execute();
   $symptomes = $query_symptomes->fetchAll();
 
   foreach( $symptomes as $noms){
-    array_push ($noms_symptomes, $noms['name']);
+    array_push ($noms_symptomes, array($noms['name'], $noms['idsymptome']));
   }
 
-  var_dump($noms_symptomes);
+  for($i = 0; $i < sizeof($mots); $i++){
+    for($j = 0; $j < sizeof($noms_symptomes); $j++) {
+      if ( 2 >= levenshtein($noms_symptomes[$j][0], $mots[$i],1, 1, 1)) {
+        array_push($correlation, $noms_symptomes[$j][0]);
+      }
+    }
+  }
+
+  $correlation = implode(',', $correlation);
+
+  $query_maladie = $bd->prepare("SELECT * FROM maladie, symptome, correlation WHERE symptome.name LIKE '%" . $correlation . "%' 
+  AND (symptome.idsymptome = correlation.idSymptome
+	AND correlation.idMaladie = maladie.idmaladie)");
+  $query_maladie->execute();
+  $maladie = $query_maladie->fetchAll();
+
+  $diagnotique = array();
+
+  foreach ($maladie as $noms_maladie){
+    array_push($diagnotique, $noms_maladie[1]);
+  }
+
+  $diagnotique = implode(',', $diagnotique);
+
+  echo 'Vous avez possiblement un/une ' . $diagnotique;
 
 }
 
